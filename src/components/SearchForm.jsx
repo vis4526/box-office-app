b1ce9c0785ff0506747ba99ceda65e4962c0f2e6
Vishesh <visheshgupta083@gmail.com>
2023-12-06 19:56:59 +0530

    installed styled components.Create global theme with sc. created custom radio.used NavLink from raect-router to style active link

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -1,5 +1,6 @@
 import { useState} from 'react';
 import { useSearchStr } from '../lib/useSearchStr';
+import  CustomRadio from './CustomRadio'
 const SearchForm = ({onSearch}) => {
     const [searchStr, setSearchStr] = useSearchStr('');
     const[searchOption,setSearchOption] = useState('shows');
@@ -10,7 +11,7 @@ const SearchForm = ({onSearch}) => {
       const onRadioChange = ev => {
         setSearchOption(ev.target.value)
       };
-      const onSubmit = (ev) => {
+      const onSubmit = ev => {
         ev.preventDefault();
        const options = {
         q: searchStr,
@@ -20,15 +21,24 @@ const SearchForm = ({onSearch}) => {
       };
     return (<form onSubmit={onSubmit}>
     <input type="text" value={searchStr} onChange={onSearchInputChange} />
-
-    <label>
-      Shows
-      <input type = "radio" name ="search-option" value="shows" checked={searchOption === 'shows'} onChange={onRadioChange} />
-              </label>
-              <label>
-      Actors
-      <input type = "radio" name ="search-option" value="actors" checked={searchOption === 'actors'} onChange={onRadioChange}/>
-              </label>
+     <CustomRadio
+     
+      label ="Shows"
+      name ="search-option" 
+     value="shows" 
+     checked={searchOption === 'shows'} 
+     onChange={onRadioChange} />
+     
+     <CustomRadio
+     
+     label ="Actors"
+     name ="search-option" 
+    value="actors" 
+    checked={searchOption === 'actors'} 
+    onChange={onRadioChange} />
+    
+              
+            
     <button type="submit">Search</button>
 </form>
     );
